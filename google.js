ca2b860ce2d3b4800095441475cb673902e83e93
venu20 <venu.w20@gmail.com>
2020-06-17 21:56:41 -0700

    1 file modified

--- google.js
+++ google.js
@@ -1,25 +1,34 @@
-class Node{
-  constructor(val){
-    this.value = val || null;
+class Node {
+  constructor(data) {
+    this.data = data;
     this.next = null;
   }
 }
-class LinkedList{
-  constructor(){
-    this.head = new Node();
+class LinkedList {
+  constructor() {
+    this.head = null;
   }
-  addItem(x){
-   if(!this.head.value){
-     this.head.value = x;
-   } else {
-     this.head.next = new Node();
-     this.head.next.value = x;
+  addItem(x) {
+    const newNode = new Node(x);
+   if(!this.head){
+     this.head = newNode;
    }
+   let tail = this.head.next;
+   if(!tail){
+     tail = newNode;
    }
+
+   this.head.next = tail
+  }
 }
 
 const ll = new LinkedList();
 ll.addItem('50');
 ll.addItem('500');
+ll.addItem('510');
+ll.addItem('530');
+ll.addItem('550');
+
+
 
 console.log(ll);
